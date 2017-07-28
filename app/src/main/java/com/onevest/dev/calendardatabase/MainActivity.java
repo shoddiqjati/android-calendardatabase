b378af27d90f40ede9c25c6d27ab5238e4f32cbb
Shoddiq Jati Premono <shoddiq.jati@gmail.com>
2017-07-28 17:48:40 +0700

    Add Calendar Request Permissions

--- app/src/main/java/com/onevest/dev/calendardatabase/MainActivity.java
+++ app/src/main/java/com/onevest/dev/calendardatabase/MainActivity.java
@@ -1,13 +1,50 @@
 package com.onevest.dev.calendardatabase;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected String[] PERMISSIONS = {Manifest.permission.READ_CALENDAR};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            requestPermissions();
+        }
+
+    }
+
+    private boolean hasPermissions(Context context, String[] permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length <= 0) {
+                requestPermissions();
+            }
+        }
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, PERMISSIONS, 1);
     }
 }
